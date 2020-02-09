f1643d0a70b14d3c4020b9c3ea6ffeb5939708e3
Andres <rixco1998@hotmail.com>
2020-02-09 15:38:23 -0500

    mas cosas

--- Calculadoracomplejos.py
+++ Calculadoracomplejos.py
@@ -36,6 +36,10 @@ def cartcom(x):
     x=q*math.cos(ang1)
     y=q*math.sin(ang1)
     return(x,y)
+def polarcart(x):
+    a=round(x[0]*math.cos(x[1]),5)
+    b=round(x[0]*math.sin(x[1]),5)
+    return(a,b)
 def fasecom(x):
     arri=x[1]
     abaj=x[0]
@@ -68,18 +72,62 @@ def multivectocom(x,a):
         z=multicom(tup,a)
         lista.append(z)
     return lista
-def tensorcom(x,y):
+def adicimatcom(x,y):
+    
+    final2=[]
+    for i in range(len(x)):
+        final=[]
+        for j in range(len(x[0])):
+            final.append(sumacom(x[i][j],y[i][j]))
+        final2.append(final)
+    return final2
+def invermatcom(x):
     final=[]
-    for i in range(len(x[0])):
-        for j in range(len(x)):
-            fina2=[]
-            for k in range(len(y[0])):
-                for l in range(len(y)):
-                    q=float((x[i][k])[0])*float((y[j][l])[0])
-                    e=(float((x[i][k])[0])*float((y[j][l])[1]))+((float((x[i][k])[1])*float((y[j][l])[0])))
-                    i=(float((x[i][k])[1])*float((y[j][l])[1])*-1)
-                    q=q+i
-                    fina2.append(q)
-            final.append(fina2)
-    print(final)
+    for i in range(len(x)):
+        fila=[]
+        for j in range(len(x[0])):
+            a=((((x[i][j])[0])*-1))
+            b=((((x[i][j])[1])*-1))
+            c=(a,b)
+            fila.append(c)
+        final.append(fila)
+    return(final)
+def escamatcom(x,y):
+    final=[]
+    for i in range(len(x)):
+        fila=[]
+        for j in range(len(x[0])):
+            a=((((x[i][j])[0])*y))
+            b=((((x[i][j])[1])*y))
+            c=(a,b)
+            fila.append(c)
+        final.append(fila)
+    return(final)
+def transmatvec(x):
+    final=[]
+    for i in range(len(x)):
+        fila=[]
+        for j in range(len(x[0])):
+            a=(((x[j][i])[0]))
+            b=(((x[j][i])[1]))
+            c=(a,b)
+            fila.append(c)
+        final.append(fila)
+    return(final)
+def conjumatveccom():
+    
+##def tensorcom(x,y):
+##    final=[]
+##    for i in range(len(x[0])):
+##        for j in range(len(x)):
+##            fina2=[]
+##            for k in range(len(y[0])):
+##                for l in range(len(y)):
+##                    q=float((x[i][k])[0])*float((y[j][l])[0])
+##                    e=(float((x[i][k])[0])*float((y[j][l])[1]))+((float((x[i][k])[1])*float((y[j][l])[0])))
+##                    i=(float((x[i][k])[1])*float((y[j][l])[1])*-1)
+##                    q=q+i
+##                    fina2.append(q)
+##            final.append(fina2)
+##    print(final)
 
--- pruebas.py
+++ pruebas.py
@@ -30,6 +30,9 @@ class Testing(unittest.TestCase):
     def testcart(self):
         a = (8,-2)
         self.assertEqual(Calculadoracomplejos.cartcom(a),(8,-2.0))
+    def testpolarcart(self):
+        a = (3,-2)
+        self.assertEqual(Calculadoracomplejos.polarcart(a),(-1.24844, -2.72789))
     def testfasecom(self):
         a = (3,-5)
         self.assertEqual(Calculadoracomplejos.fasecom(a),(-1.03))
@@ -44,10 +47,26 @@ class Testing(unittest.TestCase):
         a =[(3,-7),(2,-3.6),(5.899,-1.9),(1,2)]
         b =(-1,2)
         self.assertEqual(Calculadoracomplejos.multivectocom(a,b),[(11.0,13.0),(5.2,7.6),(-2.099,13.698),(-5.0,0)])
-    def testmultivectocom(self):
-        a =[(3,-7),(2,-3.6),(5.899,-1.9),(1,2)]
-        b =[(3,-7),(2,-3.6),(5.899,-1.9),(1,2)]
-        self.assertEqual(Calculadoracomplejos.tensorcom(a,b),[(11.0,13.0),(5.2,7.6),(-2.099,13.698),(-5.0,0)])
+    def testadicimatcom(self):
+        a =[[(3,-7),(2,-3.6)],[(5.899,-1.9),(1,2)]]
+        b =[[(3,-7),(2,-3.6)],[(5.899,-1.9),(1,2)]]
+        self.assertEqual(Calculadoracomplejos.adicimatcom(a,b),[[(6.0, -14.0), (4.0, -7.2)], [(11.798, -3.8), (2.0, 4.0)]])
+    def testinvermatcom(self):
+        a =[[(3,-7),(2,-3.6)],[(5.899,-1.9),(1,2)]]
+        self.assertEqual(Calculadoracomplejos.invermatcom(a),[[(-3, 7), (-2, 3.6)], [(-5.899, 1.9), (-1, -2)]])
+    def testescamatcom(self):
+        a =[[(6,-7),(2,-3.6)],[(5.899,-1.9),(1,2)]]
+        b=2
+        self.assertEqual(Calculadoracomplejos.escamatcom(a,b),[[(12, -14), (4, -7.2)], [(11.798, -3.8), (2, 4)]])
+    def testtransmatvec(self):
+        a =[[(6,-7),(2,-3.6),(2.5,7)],[(5.899,-1.9),(1,2),(7.8,-4)],[(6.7,8),(-1,6.9),(3.2,-2.3)]]
+        self.assertEqual(Calculadoracomplejos.transmatvec(a),[[(6, -7), (5.899, -1.9), (6.7, 8)],[(2, -3.6), (1, 2), (-1, 6.9)],[(2.5, 7), (7.8, -4), (3.2, -2.3)]])
+        
+
+##    def testmultivectocom(self):
+##        a =[(3,-7),(2,-3.6),(5.899,-1.9),(1,2)]
+##        b =[(3,-7),(2,-3.6),(5.899,-1.9),(1,2)]
+##        self.assertEqual(Calculadoracomplejos.tensorcom(a,b),[(11.0,13.0),(5.2,7.6),(-2.099,13.698),(-5.0,0)])
        
         
 if __name__ == '__main__':
